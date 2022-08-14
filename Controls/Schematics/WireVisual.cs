f6b2c9d61eca975fa4324f81dfd38fa94be8e28f
MagixGames <80378516+MagixGames@users.noreply.github.com>
2022-08-14 15:00:05 -0500

    Various fixes

--- Controls/Schematics/WireVisual.cs
+++ Controls/Schematics/WireVisual.cs
@@ -315,7 +315,6 @@ namespace LevelEditorPlugin.Controls
                     case 2: wirePen.Brush = state.WirePropertyPen.Brush; break;
                 }
             }
-            wirePen.Freeze();
 
             // highlight if overlapped by the cutting tool
             if (IsMarkedForDeletion)
@@ -335,8 +334,8 @@ namespace LevelEditorPlugin.Controls
                 b.Y += TargetPort.Rect.Location.Y + 6;
             }
 
-            Point ab = new Point(state.UseCurvedLines ? a.X + 4.5 : a.X + 9, a.Y);
-            Point ba = new Point(state.UseCurvedLines ? b.X - 4.5 : b.X - 9, b.Y);
+            Point ab = new Point(state.UseCurvedLines ? a.X : a.X + 9, a.Y);
+            Point ba = new Point(state.UseCurvedLines ? b.X : b.X - 9, b.Y);
             state.DrawingContext.DrawLine(wirePen, state.WorldMatrix.Transform(a), state.WorldMatrix.Transform(ab));
 
             if (WirePoints.Count > 0)
